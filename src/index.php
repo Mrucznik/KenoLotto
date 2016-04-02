a14a66bb4b91b9e22f9f2bde4db693149b4ac9c8
Szymon Gajda <mrucznix@gmail.com>
2016-04-02 23:20:08 +0200

    Day 1

--- src/index.php
+++ src/index.php
@@ -7,8 +7,27 @@
 <head>
     <meta charset="UTF-8">
     <title>Keno Lotto</title>
+
+<!--     Styles:-->
+    <link rel="stylesheet" href="css/Normalize.css">
+    <link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-    Elo
+    <img src="https://mysticalchrist.files.wordpress.com/2016/01/lottery-balls.jpg" class="blur" id="background-img"/>
+    <div id="top">
+        <div id="logo_container">
+
+            <img src="img/logo.png" alt="Logo"/>
+        </div>
+        <div id="menu_bar">
+            menu
+        </div>
+    </div>
+    <div id="mid">
+        mid
+    </div>
+    <div id="bot">
+        down
+    </div>
 </body>
 </html>
